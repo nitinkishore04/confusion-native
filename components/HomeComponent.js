9ef9f109453ade504d9e4c735efd1a669022ebd5
Nitin Kishore <nitin.kishore04@gmail.com>
2020-06-29 19:16:17 +0530

    Supressed Warnings using ignorewarning()

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -11,7 +11,7 @@ class Home extends Component {
     render(){
         return(
             <View>
-                <Text>Home COmponent.</Text>
+                <Text>Home Component.</Text>
             </View>
         );
     }
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
+import { YellowBox } from 'react-native';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
+YellowBox.ignoreWarnings([
+    'componentWillReceiveProps has been renamed',
+    'DrawerLayoutAndroid drawerPosition'
+])
 registerRootComponent(App);
