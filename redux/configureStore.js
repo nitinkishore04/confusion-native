860bc11dae85e692fda8ecbb63e362963161cb11
Nitin Kishore <nitin.kishore04@gmail.com>
2020-07-04 20:41:10 +0530

    Favorite Storage

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,16 +1,26 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { dishes } from './dishes';
 import { comments } from './comments';
 import { promotions } from './promotions';
 import { leaders } from './leaders';
+import { persistCombineReducers, persistStore} from 'redux-persist';
+import AsyncStorage from '@react-native-community/async-storage';
+ 
 
 import { favorites } from './favorites';
 
-export const ConfigureStore = () =>{
+export const ConfigureStore = () =>{  
+
+    const config ={
+        key: 'root',
+        storage: AsyncStorage,
+        debug: true
+    };
+
     const store = createStore(
-        combineReducers({
+        persistCombineReducers(config, {
             dishes,
             comments,
             promotions,
@@ -20,5 +30,6 @@ export const ConfigureStore = () =>{
         applyMiddleware(thunk, logger)  
     );
 
-    return store
+    const persistor = persistStore(store);
+    return { store, persistor }
 }
